bbfdcfb2474cc9ee9e0854c624e83af5b06ce210
NoistNT <arielgnr23@gmail.com>
2023-12-27 18:55:38 -0300

    Add mockup data to test

---
+++ src/data.ts
@@ -0,0 +1,42 @@
+export const gifts = [
+  {
+    id: 1,
+    name: 'PS5'
+  },
+  {
+    id: 2,
+    name: 'Xbox Series X'
+  },
+  {
+    id: 3,
+    name: 'Nintendo Switch'
+  },
+  {
+    id: 4,
+    name: 'Nintendo 3DS'
+  },
+  {
+    id: 5,
+    name: 'Nintendo 64'
+  },
+  {
+    id: 6,
+    name: 'Firetruck'
+  },
+  {
+    id: 7,
+    name: 'Police car'
+  },
+  {
+    id: 8,
+    name: 'Rally car'
+  },
+  {
+    id: 9,
+    name: 'Ambulance'
+  },
+  {
+    id: 10,
+    name: 'Airplane'
+  }
+]
