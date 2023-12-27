4b4637d8cae5dd2001991691c75cc7781804ea41
NoistNT <arielgnr23@gmail.com>
2023-12-27 18:55:07 -0300

    Add toaster from sonner

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import './index.css';
+import './index.css'
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Toaster } from 'sonner'
 
-import App from './App';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Toaster closeButton />
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+  document.getElementById('root')
+)
