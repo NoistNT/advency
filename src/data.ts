3ebd22ca1be28888bd37d67f1dd20fcec000df49
NoistNT <arielgnr23@gmail.com>
2023-12-28 18:15:18 -0300

    Add mock data

--- src/data.ts
+++ src/data.ts
@@ -1,42 +1,52 @@
 export const gifts = [
   {
     id: 1,
-    name: 'PS5'
+    name: 'PS5',
+    quantity: 1
   },
   {
     id: 2,
-    name: 'Xbox Series X'
+    name: 'Xbox Series X',
+    quantity: 1
   },
   {
     id: 3,
-    name: 'Nintendo Switch'
+    name: 'Nintendo Switch',
+    quantity: 1
   },
   {
     id: 4,
-    name: 'Nintendo 3DS'
+    name: 'Nintendo 3DS',
+    quantity: 1
   },
   {
     id: 5,
-    name: 'Nintendo 64'
+    name: 'Nintendo 64',
+    quantity: 1
   },
   {
     id: 6,
-    name: 'Firetruck'
+    name: 'Firetruck',
+    quantity: 1
   },
   {
     id: 7,
-    name: 'Police car'
+    name: 'Police car',
+    quantity: 1
   },
   {
     id: 8,
-    name: 'Rally car'
+    name: 'Rally car',
+    quantity: 1
   },
   {
     id: 9,
-    name: 'Ambulance'
+    name: 'Ambulance',
+    quantity: 1
   },
   {
     id: 10,
-    name: 'Airplane'
+    name: 'Airplane',
+    quantity: 1
   }
 ]
