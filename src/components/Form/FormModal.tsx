6721e0f8a0471cab79999fa7aa1f0ea50d40777e
NoistNT <arielgnr23@gmail.com>
2023-12-29 13:02:29 -0300

    Add modal from flowbyte

--- src/components/Form/FormModal.tsx
+++ src/components/Form/FormModal.tsx
@@ -14,18 +14,19 @@ export default function FormModal({
   return (
     <>
       <Button
-        className=""
+        className="hover:shadow-lg transition-all opacity-90"
         onClick={() => setOpenModal(true)}
-        outline
         gradientDuoTone="pinkToOrange"
       >
-        Add gift
+        <span className="text-white text-pretty text-base">Add gift</span>
       </Button>
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
-        <Modal.Header>Add a gift to the list</Modal.Header>
-        <Modal.Body>
-          <Form onAddGift={onAddGift} setOpenModal={setOpenModal} />
-        </Modal.Body>
+        <div className="backdrop-blur-sm outline outline-1 outline-white/50 bg-gradient-to-r from-amber-600 via-red-700 to-green-700 rounded-lg">
+          <Modal.Header>Add a gift to the list</Modal.Header>
+          <Modal.Body>
+            <Form onAddGift={onAddGift} setOpenModal={setOpenModal} />
+          </Modal.Body>
+        </div>
       </Modal>
     </>
   )
